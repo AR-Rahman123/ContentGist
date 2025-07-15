d46adf89cc03e07d65937996c1e7d289d00cedab
farhad <farhad@the-maliks.com>
2025-07-15 14:20:52 +0000

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import runtimeErrorOverlay from "replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    (process.env.NODE_ENV !== "production" && process.env.REPL_ID === undefined) && {
-      async configureServer() {
-        const m = await import("@replit/vite-plugin-cartographer");
-        return m.cartographer();
-      },
-    },
   ],
   resolve: {
     alias: {
